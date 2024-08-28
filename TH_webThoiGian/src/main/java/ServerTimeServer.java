81f4a006fef343dcd86f216c80cf693bef82e956
kỳ.vũ <phucky1235@gmail.com>
2024-08-28 08:09:20 +0700

    TH_01

---
+++ TH_webThoiGian/src/main/java/ServerTimeServer.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServer", urlPatterns = "/index")
+public class ServerTimeServer extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("");
+    }
+}
